83c7ce091a050f8a2242f7f563a42c618393c090
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-09 20:53:14 +0200

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -10,7 +10,7 @@ class bot(object):
     API_HASH = "ee28199396e0925f1f44d945ac174f64"
     
     # add API_TOKEN from @botfather (Mandatory)
-    API_TOKEN = "6199159516:AAGo4yNhtYzjQMFyu0ZnmeN1tDxOhHrFUbY"
+    API_TOKEN = "6037424775:AAHY1OYPXEjluwK9gtzJTzWEN2orwaGgObU"
 
 
 class dm(object):
